40856ca62370fb464876cf8d3629503cca4ffb2f
Zhenkai Weng <theoaktree1040@gmail.com>
2022-02-18 19:49:38 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Computer Vision Workshop
+This repository contains code for Wolf Corp's computer vision workshop hosted on 02/18/2022.
+
+You can view the code [here](https://github.com/wolfcorpftc/ff-cv-workshop/tree/master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode).
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+public class Auto extends LinearOpMode {
+    OpenCvWebcam webcam;
+    @Override
+    public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        Scanner scanner = new Scanner(telemetry);
+        webcam.setPipeline(scanner);
+
+        webcam.setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+            }
+        });
+        waitForStart();
+        Barcode result = scanner.getResult();
+        webcam.stopStreaming();
+        switch (result) {
+            case LEFT:
+                break;
+            case MIDDLE:
+                break;
+            case RIGHT:
+                break;
+        }
+    }
+}
